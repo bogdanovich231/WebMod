b509478b14ea34356bd8de36339636a9d16125b5
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2025-01-02 19:29:45 +0100

    feat: implemented about section n designed

---
+++ src/Components/About/About.tsx
@@ -0,0 +1,36 @@
+import styles from './About.module.css';
+import LinkedinLink from '../MediaLinks/LinkedinLink';
+import TelegramLink from '../MediaLinks/TelegramLink';
+import { useGetAboutQuery } from '../../store/api/api';
+import { extractContent } from '../../utils/ExtractContent/ExtractContent';
+import Loader from '../Loader/Loader';
+
+function About() {
+  const { data, isLoading } = useGetAboutQuery({});
+
+  const contentData = Array.isArray(data) ? data[0] : data;
+
+  if (!contentData || !contentData.content || !contentData.content.rendered) {
+    return <div>No content available</div>;
+  }
+  if (isLoading) return <Loader />;
+  const { heading, headingH3, imgSrc } = extractContent(contentData.content.rendered);
+
+  return (
+    <div className={styles.about}>
+      {heading && <h2 dangerouslySetInnerHTML={{ __html: heading }} />}
+      <div className={styles.containerAbout}>
+        <img src={imgSrc} alt="Web studio" />
+        <div className={styles.aboutTitle}>
+          {headingH3 && <h3 dangerouslySetInnerHTML={{ __html: headingH3 }} />}
+          <div className={styles.wrapperLinks}>
+            <LinkedinLink />
+            <TelegramLink />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
