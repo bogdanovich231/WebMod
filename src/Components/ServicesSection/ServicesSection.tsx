ef61c880228801f8d9660563c34bb04336610915
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2025-01-05 23:19:09 +0100

    fix: adaptive css for services n stack section

--- src/Components/ServicesSection/ServicesSection.tsx
+++ src/Components/ServicesSection/ServicesSection.tsx
@@ -36,25 +36,27 @@ function ServicesSection() {
       <h2>
         My <b className={styles.softText}>services</b>
       </h2>
-      <div className={styles.services}>
-        <div className={styles.progressBar}>
-          <div className={styles.progress} style={{ height: `${Math.max(progress, 0.15) * 100}%` }} />
-        </div>
-        <div className={styles.wrapperContent} ref={contentRef}>
-          <motion.div className={styles.content}>
-            {[...Array(4)].map((_, index) => (
-              <motion.div
-                key={index}
-                initial={{ opacity: 0, y: 50 }}
-                whileInView={{ opacity: 1, y: 0 }}
-                transition={{ duration: 1, ease: 'easeIn' }}
-                className={`${styles.contentInformation} ${activeIndex === index ? styles.active : styles.inactive}`}
-              >
-                <h3>Landing Page Development</h3>
-                <p>Stunning single-page websites designed to maximize conversions.</p>
-              </motion.div>
-            ))}
-          </motion.div>
+      <div className={styles.wrapperServices}>
+        <div className={styles.services}>
+          <div className={styles.progressBar}>
+            <div className={styles.progress} style={{ height: `${Math.max(progress, 0.15) * 100}%` }} />
+          </div>
+          <div className={styles.wrapperContent} ref={contentRef}>
+            <motion.div className={styles.content}>
+              {[...Array(4)].map((_, index) => (
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, y: 50 }}
+                  whileInView={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, ease: 'easeIn' }}
+                  className={`${styles.contentInformation} ${activeIndex === index ? styles.active : styles.inactive}`}
+                >
+                  <h3>Landing Page Development</h3>
+                  <p>Stunning single-page websites designed to maximize conversions.</p>
+                </motion.div>
+              ))}
+            </motion.div>
+          </div>
         </div>
         <StackSection />
       </div>
