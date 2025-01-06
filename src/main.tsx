010089bac8af760c616de18dc9e9af76cb0fb14f
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2025-01-06 14:14:14 +0100

    feat: lazy loading app

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
-import { StrictMode } from 'react';
+import { StrictMode, Suspense, lazy } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 import ErrorMessage from './Components/ErrorBoundary/ErrorMessage.tsx';
 import ErrorBoundary from './Components/ErrorBoundary/ErrorBoundary.tsx';
+import Loader from './Components/Loader/Loader.tsx';
+
+const App = lazy(() => import('./App.tsx'));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <ErrorBoundary fallback={<ErrorMessage />}>
-        <App />
+        <Suspense fallback={<Loader />}>
+          <App />
+        </Suspense>
       </ErrorBoundary>
     </Provider>
   </StrictMode>
